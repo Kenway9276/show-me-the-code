2389ee0321eba6e25333c4d2ad0d5ff02243b837
Kenway <454730168@qq.com>
2019-05-17 18:59:38 +0800

    1

--- 0001.py
+++ 0001.py
@@ -1,1 +1,9 @@
-print("a")
+from PIL import Image, ImageDraw, ImageFont
+
+im = Image.open("0001.jpg")
+draw = ImageDraw.Draw(im)
+font_size = im.size[0]/4
+font = ImageFont.truetype('lucon.ttf', int(font_size))
+x = im.size[0] - font_size
+draw.text((x, 0), '4', font=font, fill='red')
+im.show()
