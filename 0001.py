8214cb316a0ac2e4837b038ba53b03b0ab842888
Kenway <454730168@qq.com>
2019-05-17 19:47:06 +0800

    2019-5-17

--- 0001.py
+++ 0001.py
@@ -1,9 +1,9 @@
-from PIL import Image, ImageDraw, ImageFont
+import random, string
 
-im = Image.open("0001.jpg")
-draw = ImageDraw.Draw(im)
-font_size = im.size[0]/4
-font = ImageFont.truetype('lucon.ttf', int(font_size))
-x = im.size[0] - font_size
-draw.text((x, 0), '4', font=font, fill='red')
-im.show()
+f = open('promo_code.txt', 'w')
+for j in range(200):
+    chars = string.digits + string.ascii_uppercase
+    chosen_char = [random.choice(chars) for i in range(12)]
+    f.write(''.join(chosen_char) + '\n')
+f.close()
+
